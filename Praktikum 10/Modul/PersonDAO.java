14c68a7ed80ecc4c4dad04398d20fa8c7bd37bef
FarhanGhifari <152803166+FarhanGhifari@users.noreply.github.com>
2025-05-22 21:18:59 +0700

    Add files via upload

---
+++ Praktikum 10/Modul/PersonDAO.java
@@ -0,0 +1,10 @@
+/*
+ * File : PersonDAO.java
+ * Deskripsi : interface untuk person acces object
+ * Nama : Muhammad Farhan Al Ghifari
+ * NIM : 24060123140186
+ */
+
+ public interface PersonDAO {
+    public void savePerson(Person p) throws Exception;
+ }
